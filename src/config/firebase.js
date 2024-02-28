202feb2c82f406a929828bb44e287973826dc4d1
Harjot <harjotsk03@gmail.com>
2024-02-27 23:34:59 -0800

    trying to fix 404

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBTYf-cNmEBU7ZTGihpZWbjKGUbmilJFzI",
   authDomain: "portfolio-b7fe8.firebaseapp.com",
@@ -19,7 +14,6 @@ const firebaseConfig = {
   measurementId: "G-2PLKNKSEJV"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
